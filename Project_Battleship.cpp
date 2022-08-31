c759cf8aac06c3b267d347579d0ecc185c987503
Tiago Gimenes <tiago.scarpelin02@gmail.com>
2022-08-31 19:17:15 -0300

    Source code

---
+++ Project_Battleship.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int option = 0;
+    
+
+    while(option > 0 || option < 3){
+
+        cout << "Choose a option and press Enter!\n";
+        cin >> option;
+
+        
+        switch(option){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+        }
+
+    }
+
+    return 0;
+}
