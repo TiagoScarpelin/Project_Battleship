4d826207dc8636a2a4f86b64d7f6b3b5f2a4d114
Tiago Gimenes <tiago.scarpelin02@gmail.com>
2022-09-13 17:03:39 -0300

    Source code

--- Project_Battleship.cpp
+++ Project_Battleship.cpp
@@ -13,9 +13,9 @@ using namespace std;
 
     while(positionedQuantity < amount){
 
-        if (board[rand() % 10][rand() % 10] == 'A'){
+        if (board[rand() % 10][rand() % 10] == 'W'){
 
-            board[rand() % 10][rand() % 10] = 'P'; 
+            board[rand() % 10][rand() % 10] = 'B'; 
 
             positionedQuantity++;
         }
@@ -29,7 +29,7 @@ void startBoard(char board [10][10], char mask [10][10]){
 
     for(line = 0;line < 10; line++){
        for(column = 0;column < 10; column++){
-            board[line][column] = 'A';
+            board[line][column] = 'W';
             mask[line][column] = '*';
         } 
     }
@@ -64,6 +64,9 @@ int i;
 void displayBoard(char board [10][10], string message){
 
     int column ,line;
+    char blue[] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
+    char normal[] = { 0x1b, '[', '1', ';', '3', '9', 'm', 0 };
+    char red[] = { 0x1b, '[', '1', ';', '3', '1', 'm', 0 };
 
     cout << "\n" << message << "\n\n";
 
@@ -71,7 +74,19 @@ void displayBoard(char board [10][10], string message){
     for(line = 0;line < 10; line++){
         cout << line << "- ";
        for(column = 0;column < 10; column++){
-            cout << " " << board[line][column];
+
+            switch (board[line][column]){
+                case 'W':
+                    cout << " " <<  blue << board[line][column] << normal;
+                    break;
+                case 'B':
+                    cout << " " << red << board[line][column] << normal;
+                    break;
+                default:
+                    cout << " " << normal << board[line][column];   
+                    break;
+            }
+
         } 
         cout << "\n";
         
@@ -83,19 +98,53 @@ void checks (char board [10][10],int chosenLine, int chosenColumn, int *score, s
 
     switch(board[chosenLine][chosenColumn]){
 
-        case 'P':
+        case 'B':
             *score = *score + 10;
-            *message = "Let`s goo!!!, very good (+10 points)\n\n";
+            *message = "Let`s goo!!!, very good (+10 points)\n";
             break;
-        case 'A':
+        case 'W':
             *message = "OH, you hit de sea (;-;)\n\n";
             break;
     }
 }
 
+void About(){
+
+    system ("clear");
+    cout <<            "Game Information\n\n";
+    cout << "---------------------------------------" << endl
+         << "|                                     |" << endl
+         << "|               ABOUT                 |" << endl
+         << "|                                     |" << endl
+         << "|          Game produced by:          |" << endl
+         << "|                                     |" << endl
+         << "|   Tiago Gimenes Scarpelin           |" << endl
+         << "|                                     |" << endl
+         << "|       Produced on the date of:      |" << endl
+         << "|                                     |" << endl
+         << "|   29/08/2022                        |" << endl
+         << "|                                     |" << endl
+         << "|         With the help of:           |" << endl
+         << "|                                     |" << endl
+         << "|    Teacher John's Course at Udemy:  |" << endl
+         << "|                                     |" << endl
+         << "|  Link:https://www.udemy.com/course/ |" << endl
+         << "|  curso-completo-de-linguagem-c-     |" << endl
+         << "|  iniciante-ao-avancado/             |" << endl
+         << "|                                     |" << endl
+         << "---------------------------------------" << endl;
+
+
+    system("read -p 'Press enter to return to home menu...' var");
+}
+
+
 void game (string name)
 {
-    
+    int riskyLine [10];
+    bool test = false;
+    int riskyColumn [10];
+    int i,j=0;
     int chosenLine, chosenColumn;
     char board [10][10];
     char mask [10][10];
@@ -106,8 +155,7 @@ void game (string name)
     startBoard(board,mask);
 
     while(attempts < maxattempts){
-        
-        //displayBoard(board);
+        system("clear");
         cout << "\n";
         displayBoard(mask, message);
         cout << "\nScore: " << score;
@@ -118,12 +166,27 @@ void game (string name)
         cout << "Type a colunm " << name << "\n";
         cin >> chosenColumn;
 
-        checks(board,chosenLine,chosenColumn ,&score,&message);
+        for(i=0;i<=11;i++){
 
-        mask [chosenLine][chosenColumn] = board [chosenLine][chosenColumn];
+            if(riskyColumn[i] == chosenColumn && riskyLine[i] == chosenLine){
+                test = true;
+            }
+        }
 
-        attempts++;
-        system("clear");
+        
+        if((chosenLine > -1 && chosenLine < 11) && (chosenColumn > -1 && chosenColumn < 11) && (test == false)){
+            
+            checks(board,chosenLine,chosenColumn ,&score,&message);
+            mask [chosenLine][chosenColumn] = board [chosenLine][chosenColumn];
+            
+            riskyLine[attempts] = chosenLine;
+            riskyColumn[attempts] = chosenColumn;
+            
+            attempts++;
+        }else{
+            cout << "\nInvalid position, or repeated position. Try again!";
+            test = false;
+        }
     }   
 }
 
@@ -149,8 +212,7 @@ void homeMenu(){
                 game(name);
                 break;
             case 2:
-                
-
+                About();
                 break;
         }
 
